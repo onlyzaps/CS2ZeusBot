684f41ab317b85ca844f7c5a432e06a12f69197b
Clinton Turner <73978539+vindict6@users.noreply.github.com>
2026-03-01 01:12:52 -0700

    Update ZeusBotAI.cs

--- ZeusBotAI.cs
+++ ZeusBotAI.cs
@@ -29,7 +29,7 @@ namespace ZeusBotAI
     public class ZeusBotAIPlugin : BasePlugin
     {
         public override string ModuleName => "Zeus Bot AI (Dynamic Movement, Knives & Nades)";
-        public override string ModuleVersion => "14.0.1";
+        public override string ModuleVersion => "18.1.0";
         
         private CounterStrikeSharp.API.Modules.Timers.Timer? brainTimer;
         private readonly Dictionary<uint, CombatState> botMemory = new Dictionary<uint, CombatState>();
